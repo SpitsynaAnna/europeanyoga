1372f9317a1ba35884aeb15c634c9e9270ece100
Anna <anna.a.spitsyna@gmail.com>
2024-12-11 16:08:04 +0200

    first commit

---
+++ wp-content/uploads/mailchimp-for-wp/debug-log.php
@@ -0,0 +1,163 @@
+<?php exit; ?>
+[2024-01-17 15:06:26] WARNING: Form 8664 > gros**************@or****.fr is already subscribed to the selected list(s)
+[2024-01-17 18:50:06] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"cake********@ya****.ru","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"98.159.226.177","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"193ff5ca-4f45-3978-6255-293f954443e2"}
+[2024-01-17 18:51:15] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"cake********@ya****.ru","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"98.159.226.182","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"7dd03eb1-43c0-78ee-8fb7-e5254dde589a"}
+[2024-01-17 19:57:10] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"cake********@ya****.ru","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"98.159.226.179","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"64a3e576-30e8-450b-0f68-b546ed32aedb"}
+[2024-01-18 16:50:13] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. test@gm***.com looks fake or invalid, please enter a real email address.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"test@gm***.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"188.163.50.236","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"test@gm***.com looks fake or invalid, please enter a real email address.","instance":"b55ef6ad-42fe-1600-1444-91230dc93cdb"}
+[2024-01-18 16:50:28] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. test@gm***.com looks fake or invalid, please enter a real email address.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"test@gm***.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"188.163.50.236","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"test@gm***.com looks fake or invalid, please enter a real email address.","instance":"8c3aedae-e803-7525-3f8a-551c3a0a6067"}
+[2024-01-18 16:50:52] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. test@gm***.com looks fake or invalid, please enter a real email address.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"test@gm***.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"188.163.50.236","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"test@gm***.com looks fake or invalid, please enter a real email address.","instance":"78055825-e073-eff3-c22e-2d9d61002e56"}
+[2024-01-18 16:51:04] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. test@gm***.com looks fake or invalid, please enter a real email address.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"test@gm***.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"188.163.50.236","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"test@gm***.com looks fake or invalid, please enter a real email address.","instance":"4b65efba-379c-2980-f95f-332602c8bda8"}
+[2024-01-19 12:58:02] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"cake********@ya****.ru","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"85.203.36.57","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"76ef9c64-9fa5-90f2-d818-72b7cd9b185d"}
+[2024-01-19 23:20:27] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"cake********@ya****.ru","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"181.215.25.249","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"80e44cc5-b4ed-6633-1d3f-a82938f33996"}
+[2024-01-20 02:30:39] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"cake********@ya****.ru","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"64.64.123.35","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"cake********@ya****.ru has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"cd456361-ee46-7cd4-6941-ccdf993a61f6"}
+[2024-01-22 12:29:02] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. rahu**********@gm***.com has signed up to a lot of lists very recently; we're not allowing more signups for now
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"rahu**********@gm***.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"85.203.45.44","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"rahu**********@gm***.com has signed up to a lot of lists very recently; we're not allowing more signups for now","instance":"b5ca90ac-33aa-21ba-a349-d519a570cbe8"}
+[2024-02-29 21:41:26] WARNING: Form 8664 > gros**************@or****.fr is already subscribed to the selected list(s)
+[2024-03-10 20:25:31] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Invalid Resource. jUFj_mc4wp_3b87b250_www.europeanyoga.org@data-backup-store.com looks fake or invalid, please enter a real email address.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"jUFj_mc4wp_3b87b250_www.europeanyoga.org@data-backup-store.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"96.90.145.140","tags":[]}
+
+Response: 
+400 Bad Request
+{"type":"https://mailchimp.com/developer/marketing/docs/errors/","title":"Invalid Resource","status":400,"detail":"jUFj_mc4wp_3b87b250_www.europeanyoga.org@data-backup-store.com looks fake or invalid, please enter a real email address.","instance":"a374b02f-bdca-b9fd-353a-e93349666fc7"}
+[2024-03-29 22:23:59] WARNING: Form 8664 > valb****@ho*****.fr is already subscribed to the selected list(s)
+[2024-03-29 22:24:04] WARNING: Form 8664 > valb****@ho*****.fr is already subscribed to the selected list(s)
+[2024-04-12 14:20:20] WARNING: Form 8664 > deve**************@co******.nl is already subscribed to the selected list(s)
+[2024-04-13 07:54:05] WARNING: Form 8664 > marg******@gm*.de is already subscribed to the selected list(s)
+[2024-04-18 10:32:58] WARNING: Form 8664 > alex*******@gm***.com is already subscribed to the selected list(s)
+[2024-04-29 12:10:52] WARNING: Form 8664 > alic**************@gm***.com is already subscribed to the selected list(s)
+[2024-05-05 11:26:38] WARNING: Form 8664 > s.bo******@we*.de is already subscribed to the selected list(s)
+[2024-05-16 00:09:02] WARNING: Form 8664 > sabi**********@gm***.com is already subscribed to the selected list(s)
+[2024-05-22 12:00:07] WARNING: Form 8664 > fari***********@gm***.com is already subscribed to the selected list(s)
+[2024-06-11 19:41:14] WARNING: Form 8664 > raff****@bl*****.ch is already subscribed to the selected list(s)
+[2024-06-17 00:56:44] WARNING: Form 8664 > brun*************@ho*****.com is already subscribed to the selected list(s)
+[2024-08-02 16:20:43] WARNING: Form 8664 > flor***********@gm***.com is already subscribed to the selected list(s)
+[2024-08-12 16:06:03] WARNING: Form 8664 > mp.c******@gm***.com is already subscribed to the selected list(s)
+[2024-08-18 00:12:59] WARNING: Form 8664 > yogi*******@ma***.com is already subscribed to the selected list(s)
+[2024-08-18 20:05:23] WARNING: Form 8664 > marie-ange*******@wa*****.fr is already subscribed to the selected list(s)
+[2024-08-18 20:31:51] WARNING: Form 8664 > marie-ange*******@wa*****.fr is already subscribed to the selected list(s)
+[2024-08-27 19:10:53] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Member Exists. evel*******@bl*****.ch is already a list member. Use PUT to insert or update list members.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"evel*******@bl*****.ch","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"2a02:1210:1efb:7e00:60f7:a46e:b697:649a","tags":[]}
+
+Response: 
+400 Bad Request
+{"title":"Member Exists","status":400,"detail":"evel*******@bl*****.ch is already a list member. Use PUT to insert or update list members.","instance":"5024e527-b601-e801-1d49-4d63d1f795e6"}
+[2024-09-22 18:13:08] WARNING: Form 8664 > valb****@ho*****.fr is already subscribed to the selected list(s)
+[2024-09-23 18:04:38] WARNING: Form 8664 > flor***********@gm***.com is already subscribed to the selected list(s)
+[2024-09-24 12:47:47] WARNING: Form 8664 > vibr******@gm***.com is already subscribed to the selected list(s)
+[2024-10-25 23:31:35] WARNING: Form 8664 > pa.o*@sa**.pt is already subscribed to the selected list(s)
+[2024-11-03 13:58:05] ERROR: Form 8664 > Mailchimp API error: 400 Bad Request. Member Exists. lind*********@ou*****.com is already a list member. Use PUT to insert or update list members.
+
+Request: 
+POST https://us17.api.mailchimp.com/3.0/lists/19fa2fdfe1/members
+
+{"status":"pending","email_address":"lind*********@ou*****.com","interests":{},"merge_fields":{},"email_type":"html","ip_signup":"213.93.224.17","tags":[]}
+
+Response: 
+400 Bad Request
+{"title":"Member Exists","status":400,"detail":"lind*********@ou*****.com is already a list member. Use PUT to insert or update list members.","instance":"754946eb-a9c2-a1d3-5a3b-b2dc764a8bf5"}
